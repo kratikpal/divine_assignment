afc2e4c1a0291471811f7f8ae091e756d489ebf3
kratikpal <kratikpal@gmail.com>
2024-12-05 21:29:59 +0530

    First commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text(
+          'Home Screen',
+        ),
+      ),
+    );
+  }
+}
